e487998917ccc10415c027afa1c9d52b0c644f66
DongHeon <gjsehdrh@gmail.com>
2020-04-20 15:19:18 +0900

    실습1

---
+++ Stack.h
@@ -0,0 +1,19 @@
+#include "LinkedList.h"
+
+
+template <class T>
+class Stack : public LinkedList<T>{
+	public:
+		bool Delete (T &element){
+			if(this->first == NULL)
+                                return false;
+                        element = this->first->data;    
+                        Node<T>*i = this->first;
+                        this->first = this->first->link;
+                        delete i;
+                        this->current_size--;
+
+
+			return true;
+			}
+};
